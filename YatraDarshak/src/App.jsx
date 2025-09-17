f7620e98211008fa468777cc10ad018ed6dd9e95
jainAmishhh <jainamish1511@gmail.com>
2025-09-18 03:39:38 +0530

    added

---
+++ YatraDarshak/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Auth from './Components/AuthCredentials/Auth'
+// import AuthPage from './Components/AuthCredentials/AuthPage'
+import AuthPage from './Components/AuthCredentials/AuthPage';
+
+const App = () => {
+  return (
+    <AuthPage />
+    // <Auth />
+    // <AuthPage />
+  )
+}
+
+export default App
